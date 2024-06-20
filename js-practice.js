e17dc8bbf54c99a460cabdfe297f1caabddfb051
Smit Thummar <smitthummar28@gmail.com>
2024-06-20 09:10:38 +0530

    20-6-24

---
+++ js-practice.js
@@ -0,0 +1,19 @@
+/* String Data Type */
+
+// var a = "Hello World";
+// document.write(a);
+// document.write(typeof a);
+
+
+/* Number Data Type */
+
+// var b = 25;
+// document.write(b);
+// document.write(typeof b);
+
+
+/* Boolean Data Type */
+
+// var c = true;
+// document.write(c);
+// document.write(typeof c);
